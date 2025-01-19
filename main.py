ed5c3f06b7d01d3ac6f6cc5083d4a51bdf6ce461
Noragoto <m09091983@gmail.com>
2025-01-19 21:30:51 +0300

    Translate from image and put text in console

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import tkinter as tk
+from tkinter import messagebox
 from PIL import ImageGrab, ImageFont, Image, ImageDraw
 import io
 import easyocr
 import cv2
 import numpy as np
 from deep_translator import GoogleTranslator
+
 
 def get_screenshot_from_clipboard():
     try:
@@ -21,40 +24,57 @@ def get_screenshot_from_clipboard():
         print(f"Error when capturing an image: {e}")
         return None
 
+
 def add_text_to_image(image, text, position, font_path='arial.ttf', font_size=20, color=(255, 255, 255)):
-    # Преобразуем изображение OpenCV в PIL
     pil_image = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-
-    # Создаем объект для рисования
     draw = ImageDraw.Draw(pil_image)
-
-    # Загружаем шрифт
     font = ImageFont.truetype(font_path, font_size)
-
-    # Добавляем текст
     draw.text(position, text, font=font, fill=color)
-
-    # Преобразуем обратно в формат OpenCV
     image = cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2BGR)
-
     return image
 
-def recognize_text(image):
-    # Преоьразуем PIL изображение в np.array
-    image_np = np.array(image)
 
+def recognize_text(image):
+    image_np = np.array(image)
     reader = easyocr.Reader(['en'])
     translator = GoogleTranslator(source='en', target='ru')
 
     result = reader.readtext(image_np)
+    translated_texts = []
 
     for bbox, text, prob in result:
-        # Переводим текст
         translated_text = translator.translate(text)
-        print(translated_text)
-    return result
+        translated_texts.append(translated_text)
+
+    return translated_texts
 
 
-image = get_screenshot_from_clipboard()
-if image:
-    result = recognize_text(image)
+def on_capture_button_click():
+    image = get_screenshot_from_clipboard()
+    if image:
+        translated_texts = recognize_text(image)
+        if translated_texts:
+            text_result.delete(1.0, tk.END)  # Clearing the previous content
+            for translated_text in translated_texts:
+                text_result.insert(tk.END, f"{translated_text}\n")  # Adding the translated text
+        else:
+            messagebox.showinfo("Result", "Couldn't recognize the text")
+    else:
+        messagebox.showwarning("Warning", "The clipboard does not contain an image.")
+
+
+# Main menu
+root = tk.Tk()
+root.title("Распознавание текста с буфера обмена")
+root.geometry('600x400')
+
+# A button to capture a screenshot from the clipboard
+capture_button = tk.Button(root, text="Take text from screenshot", command=on_capture_button_click, height=1, width=25)
+capture_button.pack(pady=20)
+
+# A text field for displaying the translated text
+text_result = tk.Text(root, height=10, width=70)
+text_result.pack(pady=10)
+
+# Start GUI
+root.mainloop()
