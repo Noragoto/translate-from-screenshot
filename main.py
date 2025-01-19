2f9d12976efe98a1af04180bc926bf029d20d87f
Noragoto <m09091983@gmail.com>
2025-01-19 15:08:21 +0300

    Translate from image and put text in console

--- main.py
+++ main.py
@@ -49,26 +49,9 @@ def recognize_text(image):
     result = reader.readtext(image_np)
 
     for bbox, text, prob in result:
-        top_left, top_right, bottom_right, bottom_left = bbox
-        top_left = tuple(map(int, top_left))
-        bottom_right = tuple(map(int, bottom_right))
-
         # Переводим текст
         translated_text = translator.translate(text)
-        # Рисуем прямоугольник вокруг текста
-        image_np = cv2.rectangle(image_np, top_left, bottom_right, (0, 255, 0), 2)
-
-        # # Добавляем текст
-        # cv2.putText(image_np, text, (top_left[0], top_left[1] - 10),
-        #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (205, 90, 106), 2)
-        # Добавляем перевод
-        image_np = add_text_to_image(image_np, translated_text, (bottom_left[0], bottom_left[1] + 10), font_path='arial.ttf', font_size=20, color=(205, 90, 106))
-
-    # Отображаем измененное изображение
-    cv2.imshow("Image with text", image_np)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
+        print(translated_text)
     return result
 
 
